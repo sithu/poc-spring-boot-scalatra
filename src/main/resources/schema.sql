698c09619ce9ca0933217b70fdeb672fe1616cda
Mikael Gueck <gumi@iki.fi>
2015-11-19 12:27:45 +0200

    initial

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE restaurants (id INTEGER PRIMARY KEY, name TEXT);
+
+INSERT INTO restaurants (id, name) VALUES (1, 'Joe''s Greaser'), (2, 'Beth''s Fine Diner');
---
+++ src/main/scala/io/mikael/poc/Application.scala
@@ -0,0 +1,51 @@
+package io.mikael.poc
+
+import javax.sql.DataSource
+
+import org.json4s.{DefaultFormats, Formats}
+import org.psnively.scala.jdbc.core.JdbcTemplate
+import org.scalatra.ScalatraServlet
+import org.scalatra.json.JacksonJsonSupport
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.ServletRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Repository
+
+object Application {
+
+  def main(args: Array[String]) {
+    SpringApplication.run(classOf[Application], args:_*)
+  }
+
+}
+
+@SpringBootApplication
+class Application {
+
+  @Bean
+  def customServletRegistrationBean(customServlet : CustomServlet) : ServletRegistrationBean =
+    new ServletRegistrationBean(customServlet, "/*")
+
+  @Bean
+  def customServlet(dataSource : DataSource) : CustomServlet = new CustomServlet(dataSource)
+
+}
+
+@Repository
+class CustomServlet(dataSource : DataSource) extends ScalatraServlet with JacksonJsonSupport {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  private val jdbcTemplate = new JdbcTemplate(dataSource)
+
+  get("/") {
+    "index page"
+  }
+
+  get("/api/restaurants") {
+    contentType = formats("json")
+    jdbcTemplate.queryAndMap("SELECT name FROM restaurants")((rs, i) => { rs.getString(1) })
+  }
+
+}
